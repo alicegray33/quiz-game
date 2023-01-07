abd627f10255959d857f8c8f1ce8f8f9f221adfb
Alice Gray <witchveil@lovelace.local>
2023-01-07 09:31:22 +0000

    Reads CSV file and outputs contents

--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strings"
+)
 
+func main() {
+	problemsFile := "problems.csv"
+
+	csvFile := readFile(problemsFile)
+
+	questions := getQuestions(csvFile)
+
+	fmt.Print(questions)
+
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func readFile(problemsFile string) string {
+	csvFile, err := os.ReadFile("./" + problemsFile)
+	check(err)
+	return string(csvFile)
+}
+
+func getQuestions(problemsFile string) [][]string {
+	r := csv.NewReader(strings.NewReader(problemsFile))
+
+	questions, err := r.ReadAll()
+	check(err)
+
+	return questions
 }
